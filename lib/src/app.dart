18567b009b4d65be6c3d122f1ee93cb59bcba3e7
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-12-14 02:30:52 +0530

    statefull widgets

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -9,10 +18,14 @@ class App extends StatelessWidget {
           title: const Text("Let's See Images!"),
         ),
         body: Center(
-          child: Text("data"),
+          child: Text("Number of Images : $counter"),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
           child: Icon(Icons.add_a_photo_rounded),
           backgroundColor: Colors.red,
         ),
