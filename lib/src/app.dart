a9d6bb139ca25d41f449bfee4931298068654d09
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-12-14 02:07:44 +0530

    add

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Let's See Images!"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_a_photo_rounded),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
